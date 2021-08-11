1cd8db8deb2cf8aaf493bf4d77b6edca65b1f10b
hooke007 <hooke007@qq.com>
2021-08-11 21:30:24 +0800

    del

--- distrib/Reset_Settings.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-echo.
-echo.
-title Restore MPC Video Renderer default settings...
-start /min reg delete "HKEY_CURRENT_USER\Software\MPC-BE Filters\MPC Video Renderer" /f
-echo    settings were reset to default
-echo.
-pause >NUL
